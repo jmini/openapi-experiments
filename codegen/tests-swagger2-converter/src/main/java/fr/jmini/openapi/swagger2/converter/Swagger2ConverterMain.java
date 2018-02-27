12d864b7303df405f5a15984455739bbf91e0d43
Jeremie Bresson <dev@jmini.fr>
2018-02-27 06:32:56 +0100

    Add additional JaxRS-Codegen in converter

--- codegen/tests-swagger2-converter/src/main/java/fr/jmini/openapi/swagger2/converter/Swagger2ConverterMain.java
+++ codegen/tests-swagger2-converter/src/main/java/fr/jmini/openapi/swagger2/converter/Swagger2ConverterMain.java
@@ -4,7 +4,13 @@ import io.swagger.codegen.ClientOptInput;
 import io.swagger.codegen.CodegenConfig;
 import io.swagger.codegen.DefaultGenerator;
 import io.swagger.codegen.config.CodegenConfigurator;
+import io.swagger.codegen.languages.AbstractJavaCodegen;
+import io.swagger.codegen.languages.JavaCXFServerCodegen;
+import io.swagger.codegen.languages.JavaJAXRSCXFCDIServerCodegen;
+import io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen;
 import io.swagger.codegen.languages.JavaJerseyServerCodegen;
+import io.swagger.codegen.languages.JavaResteasyEapServerCodegen;
+import io.swagger.codegen.languages.JavaResteasyServerCodegen;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -15,10 +21,44 @@ public class Swagger2ConverterMain {
 
     public static void main(String[] args) throws IOException {
         convertJaxrs("petstore");
+        convertJaxrsCxf("petstore");
+        convertJaxrsCxfCdi("petstore");
+        convertJaxrsResteasy("petstore");
+        convertJaxrsResteasyEap("petstore");
+        convertJaxrsSpec("petstore");
     }
 
     private static void convertJaxrs(String inputSpecName) throws IOException {
         JavaJerseyServerCodegen config = new io.swagger.codegen.languages.JavaJerseyServerCodegen();
+        convertAbstractJava(inputSpecName, config);
+    }
+
+    private static void convertJaxrsCxf(String inputSpecName) throws IOException {
+        JavaCXFServerCodegen config = new io.swagger.codegen.languages.JavaCXFServerCodegen();
+        convertAbstractJava(inputSpecName, config);
+    }
+
+    private static void convertJaxrsCxfCdi(String inputSpecName) throws IOException {
+        JavaJAXRSCXFCDIServerCodegen config = new io.swagger.codegen.languages.JavaJAXRSCXFCDIServerCodegen();
+        convertAbstractJava(inputSpecName, config);
+    }
+
+    private static void convertJaxrsResteasy(String inputSpecName) throws IOException {
+        JavaResteasyServerCodegen config = new io.swagger.codegen.languages.JavaResteasyServerCodegen();
+        convertAbstractJava(inputSpecName, config);
+    }
+
+    private static void convertJaxrsResteasyEap(String inputSpecName) throws IOException {
+        JavaResteasyEapServerCodegen config = new io.swagger.codegen.languages.JavaResteasyEapServerCodegen();
+        convertAbstractJava(inputSpecName, config);
+    }
+
+    private static void convertJaxrsSpec(String inputSpecName) throws IOException {
+        JavaJAXRSSpecServerCodegen config = new io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen();
+        convertAbstractJava(inputSpecName, config);
+    }
+
+    private static void convertAbstractJava(String inputSpecName, AbstractJavaCodegen config) throws IOException {
         final String artifactId = toArtifactId(inputSpecName, config);
         config.setArtifactId(artifactId);
         config.setJava8Mode(true);
@@ -29,12 +69,15 @@ public class Swagger2ConverterMain {
         final String folder = "../2.0/" + inputSpecName;
         final String subFolder = toArtifactId(inputSpecName, config);
         final String outputDir = folder + "/" + subFolder;
+        Path outputDirPath = Paths.get(outputDir);
 
-        Files.walk(Paths.get(outputDir))
-            .filter(Files::isRegularFile)
-            .filter(p -> !".project".equals(p.getFileName().toString()) && !".classpath".equals(p.getFileName().toString()) && !p.getFileName().toString().endsWith(".prefs"))
-            .map(Path::toFile)
-            .forEach(File::delete);
+        if(Files.exists(outputDirPath)) {
+            Files.walk(outputDirPath)
+                .filter(Files::isRegularFile)
+                .filter(p -> !".project".equals(p.getFileName().toString()) && !".classpath".equals(p.getFileName().toString()) && !p.getFileName().toString().endsWith(".prefs"))
+                .map(Path::toFile)
+                .forEach(File::delete);
+        }
 
         CodegenConfigurator configurator = new CodegenConfigurator();
         configurator.setLang(config.getName()); 
@@ -44,8 +87,8 @@ public class Swagger2ConverterMain {
         input.setConfig(config);
 
         new DefaultGenerator().opts(input).generate();
-        
-        removeGeneratedAnnotationInJavaFiles(Paths.get(outputDir));
+
+        removeGeneratedAnnotationInJavaFiles(outputDirPath);
     }
 
     private static String toArtifactId(String inputSpecName, CodegenConfig config) {
--- codegen/tests-swagger3-converter/src/main/java/fr/jmini/openapi/swagger3/converter/Swagger3ConverterMain.java
+++ codegen/tests-swagger3-converter/src/main/java/fr/jmini/openapi/swagger3/converter/Swagger3ConverterMain.java
@@ -4,7 +4,13 @@ import io.swagger.codegen.ClientOptInput;
 import io.swagger.codegen.CodegenConfig;
 import io.swagger.codegen.DefaultGenerator;
 import io.swagger.codegen.config.CodegenConfigurator;
+import io.swagger.codegen.languages.java.AbstractJavaCodegen;
+import io.swagger.codegen.languages.java.JavaCXFServerCodegen;
+import io.swagger.codegen.languages.java.JavaJAXRSCXFCDIServerCodegen;
+import io.swagger.codegen.languages.java.JavaJAXRSSpecServerCodegen;
 import io.swagger.codegen.languages.java.JavaJerseyServerCodegen;
+import io.swagger.codegen.languages.java.JavaResteasyEapServerCodegen;
+import io.swagger.codegen.languages.java.JavaResteasyServerCodegen;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -15,10 +21,44 @@ public class Swagger3ConverterMain {
 
     public static void main(String[] args) throws IOException {
         convertJaxrs("petstore");
+        convertJaxrsCxf("petstore");
+        convertJaxrsCxfCdi("petstore");
+        convertJaxrsResteasy("petstore");
+        convertJaxrsResteasyEap("petstore");
+        convertJaxrsSpec("petstore");
     }
 
     private static void convertJaxrs(String inputSpecName) throws IOException {
         JavaJerseyServerCodegen config = new io.swagger.codegen.languages.java.JavaJerseyServerCodegen();
+        convertAbstractJava(inputSpecName, config);
+    }
+
+    private static void convertJaxrsCxf(String inputSpecName) throws IOException {
+        JavaCXFServerCodegen config = new io.swagger.codegen.languages.java.JavaCXFServerCodegen();
+        convertAbstractJava(inputSpecName, config);
+    }
+
+    private static void convertJaxrsCxfCdi(String inputSpecName) throws IOException {
+        JavaJAXRSCXFCDIServerCodegen config = new io.swagger.codegen.languages.java.JavaJAXRSCXFCDIServerCodegen();
+        convertAbstractJava(inputSpecName, config);
+    }
+
+    private static void convertJaxrsResteasy(String inputSpecName) throws IOException {
+        JavaResteasyServerCodegen config = new io.swagger.codegen.languages.java.JavaResteasyServerCodegen();
+        convertAbstractJava(inputSpecName, config);
+    }
+
+    private static void convertJaxrsResteasyEap(String inputSpecName) throws IOException {
+        JavaResteasyEapServerCodegen config = new io.swagger.codegen.languages.java.JavaResteasyEapServerCodegen();
+        convertAbstractJava(inputSpecName, config);
+    }
+
+    private static void convertJaxrsSpec(String inputSpecName) throws IOException {
+        JavaJAXRSSpecServerCodegen config = new io.swagger.codegen.languages.java.JavaJAXRSSpecServerCodegen();
+        convertAbstractJava(inputSpecName, config);
+    }
+
+    private static void convertAbstractJava(String inputSpecName, AbstractJavaCodegen config) throws IOException {
         final String artifactId = toArtifactId(inputSpecName, config);
         config.setArtifactId(artifactId);
         config.setJava8Mode(true);
@@ -29,12 +69,15 @@ public class Swagger3ConverterMain {
         final String folder = "../3.0/" + inputSpecName;
         final String subFolder = toArtifactId(inputSpecName, config);
         final String outputDir = folder + "/" + subFolder;
+        Path outputDirPath = Paths.get(outputDir);
 
-        Files.walk(Paths.get(outputDir))
-            .filter(Files::isRegularFile)
-            .filter(p -> !".project".equals(p.getFileName().toString()) && !".classpath".equals(p.getFileName().toString()) && !p.getFileName().toString().endsWith(".prefs"))
-            .map(Path::toFile)
-            .forEach(File::delete);
+        if(Files.exists(outputDirPath)) {
+            Files.walk(outputDirPath)
+                .filter(Files::isRegularFile)
+                .filter(p -> !".project".equals(p.getFileName().toString()) && !".classpath".equals(p.getFileName().toString()) && !p.getFileName().toString().endsWith(".prefs"))
+                .map(Path::toFile)
+                .forEach(File::delete);
+        }
 
         CodegenConfigurator configurator = new CodegenConfigurator();
         configurator.setLang(config.getName()); 
@@ -45,7 +88,7 @@ public class Swagger3ConverterMain {
 
         new DefaultGenerator().opts(input).generate();
 
-        removeGeneratedAnnotationInJavaFiles(Paths.get(outputDir));
+        removeGeneratedAnnotationInJavaFiles(outputDirPath);
     }
 
     private static String toArtifactId(String inputSpecName, CodegenConfig config) {

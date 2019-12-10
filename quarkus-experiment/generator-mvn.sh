35466f3d3acca990af8d0c6368867d349cfc629a
Jeremie Bresson <dev@jmini.fr>
2019-12-10 07:20:08 +0100

    Update Quarkus to 1.0.1.Final

--- quarkus-experiment/generator-mvn.sh
+++ quarkus-experiment/generator-mvn.sh
@@ -2,7 +2,7 @@
 
 rm -rf openapi-jaxrs-server/
 
-mvn io.quarkus:quarkus-maven-plugin:1.0.0.Final:create \
+mvn io.quarkus:quarkus-maven-plugin:1.0.1.Final:create \
   -DprojectGroupId=org.openapitools \
   -DprojectArtifactId=openapi-jaxrs-server \
   -DclassName="org.openapi.openapitools.PingApi" \
